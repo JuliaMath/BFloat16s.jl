4a2eb12b5dd19f203ad710b0ead7c368b4d17918
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-09-07 03:02:23 -0400

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,6 +9,14 @@ bf1 = BFloat16(1.0)
 bf2 = BFloat16(2.0)
 bf3 = BFloat16(3.0)
 bf4 = BFloat16(4.0)
+bfneg1 = BFloat16(-1.0)
+
+@test bfhalf == bfhalf
+@test bf1 != bf2
+@test bf1 > bfneg1
+@test bf1 >= bfneg1
+@test bf2 > bfneg1
+@test bf2 >= bf1
 
 @test bf1 + bf2 == bf3
 @test bf2 - bf1 == bf1
