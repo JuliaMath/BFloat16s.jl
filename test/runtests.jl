84e55721f330d283b9f1fb1cf33951973c07c184
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2018-09-07 00:39:07 -0400

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,10 @@
 using BFloat16s
 using Test
 
+half = 0.5f0
+two  = 2.0f0
+
+bfhalf = BFloat16(0.5)
 bf1 = BFloat16(1.0)
 bf2 = BFloat16(2.0)
 bf3 = BFloat16(3.0)
@@ -10,3 +14,22 @@ bf4 = BFloat16(4.0)
 @test bf2 - bf1 == bf1
 @test bf2 * bf2 == bf4
 @test bf4 / bf2 == bf2
+
+for F in (:sqrt, :cbrt, :exp, :expm1, :log, :log1p, :log10, :log2,
+          :sin, :cos, :tan, :csc, :sec, :cot,
+          :asin, :acos, :atan, :acot,
+          :sinh, :cosh, :tanh, :csch, :sech, :coth,
+          :asinh, :atanh, :acsch, :asech)
+  @eval begin
+    @test isapprox($F(bfhalf), $F(half))
+  end
+end
+
+for F in (:acsc, :asec,
+          :acosh,:acoth)
+  @eval begin
+    @test isapprox($F(bf2), $F(two))
+  end
+end
+
+    
