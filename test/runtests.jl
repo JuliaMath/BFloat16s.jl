a32492ff13c66add607c20bc36b1db684a9de6e1
Alexis Montoison <alexis.montoison@polymtl.ca>
2019-10-22 22:46:40 -0400

    Add eps functions and some little improvements

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,19 +1,31 @@
 using Test, BFloat16s
 
-@test BFloat16(1)   <  BFloat16(2)
-@test BFloat16(1f0) <  BFloat16(2f0)
-@test BFloat16(1.0) <  BFloat16(2.0)
-@test BFloat16(1)   <= BFloat16(2)
-@test BFloat16(1f0) <= BFloat16(2f0)
-@test BFloat16(1.0) <= BFloat16(2.0)
-@test BFloat16(2)   >  BFloat16(1)
-@test BFloat16(2f0) >  BFloat16(1f0)
-@test BFloat16(2)   >= BFloat16(1)
-@test BFloat16(2f0) >= BFloat16(1f0)
-@test BFloat16(2.0) >= BFloat16(1.0)
+@testset "comparisons" begin
+	@test BFloat16(1)   <  BFloat16(2)
+	@test BFloat16(1f0) <  BFloat16(2f0)
+	@test BFloat16(1.0) <  BFloat16(2.0)
+	@test BFloat16(1)   <= BFloat16(2)
+	@test BFloat16(1f0) <= BFloat16(2f0)
+	@test BFloat16(1.0) <= BFloat16(2.0)
+	@test BFloat16(2)   >  BFloat16(1)
+	@test BFloat16(2f0) >  BFloat16(1f0)
+	@test BFloat16(2.0) >  BFloat16(1.0)
+	@test BFloat16(2)   >= BFloat16(1)
+	@test BFloat16(2f0) >= BFloat16(1f0)
+	@test BFloat16(2.0) >= BFloat16(1.0)
+	@test BFloat16(2)   != BFloat16(1)
+	@test BFloat16(2f0) != BFloat16(1f0)
+	@test BFloat16(2.0) != BFloat16(1.0)
+end
 
-@test abs(BFloat16(-10)) == BFloat16(10)
-@test Float32(BFloat16(10)) == 1f1
-@test Float64(BFloat16(10)) == 10.0
-@test BFloat16(2) ^ BFloat16(4) == BFloat16(16)
-@test sqrt(BFloat16(4f0)) == BFloat16(2f0)
+@testset "conversions" begin
+	@test Float32(BFloat16(10)) == 1f1
+	@test Float64(BFloat16(10)) == 10.0
+end
+
+@testset "functions" begin
+	@test abs(BFloat16(-10)) == BFloat16(10)
+	@test BFloat16(2) ^ BFloat16(4) == BFloat16(16)
+	@test eps(BFloat16) == BFloat16(0.0078125)
+	@test sqrt(BFloat16(4f0)) == BFloat16(2f0)
+end
