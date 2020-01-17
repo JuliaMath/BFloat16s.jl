48173981b2bd0d7cde90287b57a734c6796bebf1
Alexis Montoison <alexis.montoison@polymtl.ca>
2020-01-18 00:54:14 +0100

    Test conversions to Integer and round functions

--- test/runtests.jl
+++ test/runtests.jl
@@ -21,6 +21,8 @@ end
 @testset "conversions" begin
 	@test Float32(BFloat16(10)) == 1f1
 	@test Float64(BFloat16(10)) == 10.0
+	@test Int32(BFloat16(10)) == Int32(10)
+	@test Int64(BFloat16(10)) == Int64(10)
 end
 
 @testset "functions" begin
@@ -28,4 +30,8 @@ end
 	@test BFloat16(2) ^ BFloat16(4) == BFloat16(16)
 	@test eps(BFloat16) == BFloat16(0.0078125)
 	@test sqrt(BFloat16(4f0)) == BFloat16(2f0)
+	@test round(BFloat16(10.4), RoundUp) == BFloat16(11.0)
+	@test round(BFloat16(10.6), RoundDown) == BFloat16(10.0)
+	@test round(BFloat16(3.2), RoundNearest) == BFloat16(3.0)
+	@test round(BFloat16(4.8), RoundNearest) == BFloat16(5.0)
 end
