7a94b5cdb62beeab984c2994a012d0e335fa7dcb
Brian Chen <ToucheSir@users.noreply.github.com>
2022-01-03 19:40:58 -0800

    Remove `ini_hex` and `ini_HEX`
    Ref. https://github.com/JuliaLang/julia/pull/32859, https://github.com/JuliaMath/DoubleFloats.jl/pull/125. This is causing warnings downstream in CUDA.jl.

--- src/printf.jl
+++ src/printf.jl
@@ -1,4 +1,4 @@
-import Printf: ini_dec, fix_dec, ini_hex, ini_HEX
+import Printf: ini_dec, fix_dec
 
 if VERSION < v"1.1"
   fix_dec(out, d::BFloat16,               flags::String, width::Int, precision::Int, c::Char) = fix_dec(out, Float32(d),          flags, width, precision, c)
@@ -10,8 +10,4 @@ if VERSION < v"1.1"
 else
   fix_dec(out, d::BFloat16,               flags::String, width::Int, precision::Int, c::Char, digits) = fix_dec(out, Float32(d),          flags, width, precision, c, digits)
   ini_dec(out, d::BFloat16, ndigits::Int, flags::String, width::Int, precision::Int, c::Char, digits) = ini_dec(out, Float32(d), ndigits, flags, width, precision, c, digits)
-  ini_hex(out, d::BFloat16, ndigits::Int, flags::String, width::Int, precision::Int, c::Char, digits) = ini_hex(out, Float32(d), ndigits, flags, width, precision, c, digits)
-  ini_HEX(out, d::BFloat16, ndigits::Int, flags::String, width::Int, precision::Int, c::Char, digits) = ini_HEX(out, Float32(d), ndigits, flags, width, precision, c, digits)
-  ini_hex(out, d::BFloat16,               flags::String, width::Int, precision::Int, c::Char, digits) = ini_hex(out, Float32(d),          flags, width, precision, c, digits)
-  ini_HEX(out, d::BFloat16,               flags::String, width::Int, precision::Int, c::Char, digits) = ini_HEX(out, Float32(d),          flags, width, precision, c, digits)
 end
