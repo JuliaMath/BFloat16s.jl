c911f2c7144411b065cd307ec41edfa2db9d3585
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2022-09-19 06:09:41 -0400

    test frexp, ldexp, exponent, significand

--- test/structure.jl
+++ test/structure.jl
@@ -18,8 +18,17 @@ end
 @testset "BFloat16 parts" begin
   @test exponent(whole) == 0
   @test significand(whole) == zero(BFloat16)
-  @test frexp(phi) == (BFloat16(0.30859375), 1)
-  @test ldexp(BFloat16(0.30859375), 1) == phi
+  
+  @test frexp(phi) == (BFloat16(0.80859375), 1)
+  @test ldexp(BFloat16(0.80859375), 1) == phi
+  
+  @test exponent(invphi3) == -3
+  @test significand(invphi3) == BFloat16(1.8828125)
+  
+  fr,xp = frexp(invphi3)
+  @test xp == -2
+  @test fr == BFloat16(0.94140625)
+  @test ldexp(fr, xp) == invphi3
 end
 
 
